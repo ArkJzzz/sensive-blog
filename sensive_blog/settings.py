9756f1ff13fe9796b007b3aaca9f1c17b28ac54d
ArkJzzz <arkjzzz@gmail.com>
2021-09-01 19:39:46 +0300

    Setup debug toolbar

--- sensive_blog/settings.py
+++ sensive_blog/settings.py
@@ -20,7 +20,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'debug_toolbar',
     'blog',
 ]
 
@@ -32,14 +32,17 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'sensive_blog.urls'
 
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
 STATIC_ROOT = ''
 
 TEMPLATES = [
@@ -92,6 +95,11 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
+
 
 STATIC_URL = '/static/'
 
